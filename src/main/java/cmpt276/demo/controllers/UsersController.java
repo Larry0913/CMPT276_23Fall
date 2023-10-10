f9768ef73bcae10ae3a734ecc262824a7edbcc91
Larry <lingjiel@sfu.ca>
2023-10-10 02:43:14 -0700

    add new login and logout function

--- src/main/java/cmpt276/demo/controllers/UsersController.java
+++ src/main/java/cmpt276/demo/controllers/UsersController.java
@@ -6,10 +6,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 import cmpt276.demo.models.User;
 import cmpt276.demo.models.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.util.List;
 import java.util.Map;
@@ -28,6 +31,11 @@ public class UsersController {
         return "users/showAll";
     }
 
+    @GetMapping("/")
+    public RedirectView process() {
+        return new RedirectView("login");
+    }
+
     @PostMapping("/users/add")
     public String addUser(@RequestParam Map<String, String> newuser, HttpServletResponse response) {
         System.out.println("Add user");
@@ -38,5 +46,41 @@ public class UsersController {
         userRepo.save(new User(newName, newPwd, newAdmin));
         response.setStatus(201);
         return "users/addedUser";
+    }
+
+    @GetMapping("/login")
+    public String getLogin(Model model, HttpServletRequest request, HttpSession session) {
+        User user = (User) session.getAttribute("session-user");
+        if (user == null) {
+            return "users/login";
+        }
+        else {
+            model.addAttribute("user", user);
+            return "users/protected";
+        }
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam Map<String, String> formData, Model model, HttpServletRequest request, HttpSession session) {
+        // processing lohin
+        String name = formData.get("name");
+        String pwd = formData.get("password");
+        List<User> userlist = userRepo.findByUsernameAndPassword(name, pwd);
+        if (userlist.isEmpty()) {
+            return "users/login";
+        }
+        else {
+            // successfully login
+            User user = userlist.get(0);
+            request.getSession().setAttribute("session_user", user);
+            model.addAttribute("user", user);
+            return "users/protected";
+        }
+    }
+
+    @GetMapping("/logout")
+    public String destoySession(HttpServletRequest request) {
+        request.getSession().invalidate();
+        return "users/login";
     }
 }
