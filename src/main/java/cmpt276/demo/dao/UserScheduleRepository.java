bae068a700acc3016bf5c000c0b5e0d1b095302c
Ali <xatrus.mozneb@gmail.com>
2023-10-26 17:23:39 -0700

    can search userSchedule repo by user and week

--- src/main/java/cmpt276/demo/dao/UserScheduleRepository.java
+++ src/main/java/cmpt276/demo/dao/UserScheduleRepository.java
@@ -1,9 +1,15 @@
 package cmpt276.demo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import cmpt276.demo.models.User;
 import cmpt276.demo.models.UserSchedule;
+import cmpt276.demo.models.Week;
 
 public interface UserScheduleRepository extends JpaRepository<UserSchedule, Integer> {
+    
+    List<UserSchedule> findByUserAndWeek(User user,Week week);
 
 }
